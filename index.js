cd678bf953eddce999c40417d6b2f3d8e529d6bc
sanderref <44145107+sanderref@users.noreply.github.com>
2021-07-17 18:42:52 +0200

    basic commands
    ready for first deploy

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const chessAPI = new ChessWebAPI();
 
 const client = new Discord.Client();
 
-
-
+//#region - functions to be seperated into modules ----------------------------------
+//-----------------------------------------------------------------------------------
 const getPlayerData = async (player) => {
 
     let player_name;
@@ -50,14 +50,54 @@ const getPlayerData = async (player) => {
 
 }
 
-//discord bot events
+const channelExists = (guild, name) => {
+    let channels = guild.channels.cache;
+    if (channels.some((e) => {
+        if (e.name === name) {
+            console.log(`channel ${name} found`);
+            return true;
+        }
+    })) {
+        return true;
+    }
+
+    console.log(`no channel named ${name} was found`);
+    return false;
+}
+
+const roleExists = (guild, name) => {
+    let roles = guild.roles.cache;
+    if (roles.some((e) => {
+        if (e.name === name) {
+            console.log(`role ${name} found`);
+            return true;
+        }
+    })) {
+        return true;
+    }
+
+    console.log(`no role named ${name} was found`);
+    return false;
+}
+
+const giveRole = (roleID, member) => {
+    let role = member.guild.roles.cache.find(r => r.name === roleID);
+    member.roles.add(role);
+    console.log(`${member.user.username} recieved the role ${role.name}`);
+}
+//#endregion
+
+//#region - discord bot events ------------------------------------------------------
+//-----------------------------------------------------------------------------------
 const commandFlag = '!';
 
 
 client.login(process.env.BOT_TOKEN);
 
+
 client.once('ready', () => {
     console.log('[bot online]...');
+    client.user.setActivity('chess to make fewer mistakes');
 });
 
 client.on('message', async (msg) => {
@@ -72,11 +112,83 @@ client.on('message', async (msg) => {
     }
 });
 
-//commands
+client.on('guildMemberAdd', async (member) => {
+
+    console.log(member.user.username + ' joined the guild');
+
+    let role = member.guild.roles.cache.find(r => r.name === process.env.NEW_MEMBER_ROLE);
+    if (!role) return console.log("Role does not exist.");
+    member.roles.add(role);
+
+    const channel = member.guild.channels.cache.find(ch => ch.name === process.env.NEW_MEMBER_CHANNEL_NAME);
+    if (!channel) return console.log("Channel does not exist.");
+
+
+    let msg = await channel.send(`welcome ${member.user.username}. Everyone wave!`);
+    await msg.react('👋');
+
+    giveRole(process.env.NEW_MEMBER_ROLE, member);
+});
+//#endregion
+
+//#region - commands ----------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 handleCommand = async (commandArr, message) => {
+
     command = commandArr[0].substr(1);
     params = commandArr.slice(1);
-    console.log(params[0]);
+
+    if (command === 'help') {
+        fs.readFile('help-list.txt', 'utf8', (err, data) => {
+            if (err) throw err;
+            let helpList = "\n" + data;
+            message.reply(helpList);
+        });
+        return
+    }
+
+    if (command === 'setup') {
+        if (message.member.hasPermission("ADMINISTRATOR")) {
+            let guild = message.guild;
+
+            if (!channelExists(guild, process.env.NEW_MEMBER_CHANNEL_NAME)) {
+                guild.channels.create(process.env.NEW_MEMBER_CHANNEL_NAME, { reason: 'channel did not exist' });
+            } else {
+                console.log(`the ${process.env.NEW_MEMBER_CHANNEL_NAME} channel existed, and it was therefore not created`);
+            }
+
+            if (!roleExists(guild, process.env.NEW_MEMBER_ROLE)) {
+                guild.roles.create({
+                    data: {
+                        name: 'Pawn',
+                        color: '#ffffff',
+                    },
+                    reason: 'role did not exist',
+                })
+            } else {
+                console.log(`the ${process.env.NEW_MEMBER_ROLE} role existed, and it was therefore not created`);
+            }
+
+
+            let guildMembers = guild.members.cache;
+            //console.log(guildMembers);
+
+            guildMembers.some((member) => {
+                if (member.roles.cache.some((role) => {
+                    if (role.name === process.env.NEW_MEMBER_ROLE) {
+                        return true;
+                    }
+                })) {
+                    console.log(`user already has that role!`);
+                } else {
+                    console.log(`role not found on user`);
+                    giveRole(process.env.NEW_MEMBER_ROLE, member);
+                }
+            });
+
+        }
+        return
+    }
 
     if (command === 'player') {
         let playerData = await getPlayerData(params[0]);
@@ -87,5 +199,26 @@ handleCommand = async (commandArr, message) => {
         await message.reply(
             `${player_title}${player_name} currently has a rating of ${rapid_rating} in rapid`
         );
+        return
+    }
+
+    //TODO: implement customization
+    if (command === 'joke') {
+        message.reply('I had dinner once with a Chess Grand Master in a restaurant with checked tablecloths..');
+        setTimeout(() => {
+            message.reply('It took him two hours to pass me the salt.');
+            return
+        }, 4000
+        );
+    }
+
+    if (command === 'test') {
+        if (
+            channelExists(message.guild, process.env.NEW_MEMBER_CHANNEL_NAME) &&
+            roleExists(message.guild, process.env.NEW_MEMBER_ROLE)
+        ) {
+            console.log('stuff exists');
+        }
     }
 }
+//#endregion
